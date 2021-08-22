b886e28ba88f81bd00a6b3c1a996a2f84cbe4f63
yuanyumi <yuqi_yuan@outlook.com>
2021-08-22 11:16:32 +0800

    Create _0611.java

---
+++ Classification/Two Pointers/0611.Valid-Triangle-Number/_0611.java
@@ -0,0 +1,32 @@
+package two_points;
+
+import java.util.Arrays;
+
+public class _0611 {
+
+	public static void main(String[] args) {
+		Solution0611 a = new Solution0611();
+		int[] b = {2,2,3,4};
+		System.out.println(a.triangleNumber(b));
+	}
+
+}
+class Solution0611 {
+    public int triangleNumber(int[] nums) {
+    	int ans = 0;
+    	Arrays.sort(nums);
+    	for(int i = 0; i < nums.length; i ++) {
+    		for(int j = i + 1; j < nums.length; j ++) {
+    			for(int k = j + 1; k < nums.length; k ++) {
+    				if(nums[i] + nums[j] > nums[k]) {
+    					System.out.println(nums[i] + " " + nums[j] + " " + nums[k]);
+    					ans ++;
+    				}
+    				else break;
+    			}
+    		}
+    	}
+    	
+    	return ans;
+    }
+}
