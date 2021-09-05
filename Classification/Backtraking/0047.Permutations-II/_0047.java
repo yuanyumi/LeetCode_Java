9e3a45f2549b1e1c4a123ff1fc94dcf9a5dbd818
yuanyumi <yuqi_yuan@outlook.com>
2021-09-05 13:45:40 +0800

    Create _0047.java

---
+++ Classification/Backtraking/0047.Permutations-II/_0047.java
@@ -0,0 +1,34 @@
+package Backtraking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _0047 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Solution0047 {
+	List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+    	dfs(0, nums);
+    	return ans;
+    }
+	private void dfs(int k, int[] nums) {
+		if(k >= nums.length) {
+			List<Integer> l = new ArrayList<>();
+			for(Integer n : nums) l.add(n);
+			if(!ans.contains(l)) ans.add(l);
+			return;
+		}
+		for(int i = k; i < nums.length; i ++) {
+			int t = nums[i]; nums[i] = nums[k]; nums[k] = t;
+			dfs(k + 1, nums);
+			t = nums[i]; nums[i] = nums[k]; nums[k] = t;
+		}
+	}
+}
